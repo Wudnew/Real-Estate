d8de4fbae1ae9c3a15cff9626ec708a4a637aca6
Wudnew <99720690+Wudnew@users.noreply.github.com>
2024-11-20 18:07:30 +0300

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,100 @@
+import logo from './logo.svg'
+import logo_dark from './logo_dark.svg'
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import brand_img from './brand_img.png'
+import project_img_1 from './project_img_1.jpg'
+import project_img_2 from './project_img_2.jpg'
+import project_img_3 from './project_img_3.jpg'
+import project_img_4 from './project_img_4.jpg'
+import project_img_5 from './project_img_5.jpg'
+import project_img_6 from './project_img_6.jpg'
+import testimony_img_1 from './testimony1.jfif'
+import testimony_img_2 from './testimony2.jfif'
+import testimony_img_3 from './testimony3.jfif'
+
+
+export const assets = {
+    logo,
+    logo_dark,
+    cross_icon,
+    menu_icon,
+    star_icon,
+    brand_img,
+    project_img_1,
+    project_img_2,
+    project_img_3,
+    project_img_4,
+    left_arrow,
+    right_arrow,
+}
+
+export const projectsData = [
+    {
+      title: "Skyline Haven",
+      price: "$2,50,000",
+      location: "California",
+      image: project_img_1
+    },
+    {
+      title: "Vista Verde",
+      price: "$2,50,000",
+      location: "San Francisco",
+      image: project_img_2
+    },
+    {
+      title: "Serenity Suites",
+      price: "$2,50,000",
+      location: "Chicago",
+      image: project_img_3
+    },
+    {
+      title: "Central Square",
+      price: "$2,50,000",
+      location: "Los Angeles",
+      image: project_img_4
+    },
+    {
+      title: "Vista Verde",
+      price: "$2,50,000",
+      location: "San Francisco",
+      image: project_img_5
+    },
+    {
+      title: "Serenity Suites",
+      price: "$2,50,000",
+      location: "Chicago",
+      image: project_img_6
+    },
+    
+  ];
+
+  export const testimonialsData = [
+    {
+        name: "Carlos Tevez",
+        title: "CEO",
+        image: testimony_img_1,
+        alt: "Carlos Tevez testimonies",
+        rating: 5,
+        text: "Working with this real estate team was an absolute pleasure! They made the home-buying process seamless and stress-free. From understanding my needs to finding the perfect property, their professionalism and dedication truly stood out. I felt supported at every step, and I’m so grateful for their expertise. Highly recommend for anyone looking to buy or sell!"
+    },
+    {
+        name: "Charles Schols",
+        title: "Marketing Manager",
+        image: testimony_img_2,
+        alt: "Charles Schols testimonies",
+        rating: 4,
+        text: "I couldn’t have asked for a better experience! From the moment I reached out to this team, I was impressed with their attention to detail and responsiveness. They helped me navigate the market and secure a beautiful home. I felt confident and informed throughout the entire process. This team made my dream home a reality!"
+    },
+    {
+        name: "Sara Zeleniski",
+        title: "General Manager",
+        image: testimony_img_3,
+        alt: "Sara Zeleniski testimonies",
+        rating: 5,
+        text: "The team exceeded all of my expectations when it came to finding the right home for me. They listened to my preferences, provided invaluable advice, and showed me several wonderful options. Their commitment to client satisfaction was evident, and I couldn’t be happier with the outcome. It was a pleasure working with such dedicated professionals!"
+    }
+];
