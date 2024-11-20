f1ffc34cb8ca5400efd01a9ccad5624eb50b1eb5
Wudnew <99720690+Wudnew@users.noreply.github.com>
2024-11-20 18:48:33 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar() {
         <img src={assets.logo} alt=""/>
         <ul className='hidden md:flex gap-7 text-white'>
             <a href='#Header' className='cursor-pointer hover:text-gray-400'>Home</a>
-             <a href='#About' className='cursor-pointer hover:text-gray-400'>About</a>
+             <a href='#About' className='cursor-pointer hover:text-gray-400'>About Us</a>
               <a href='#Projects' className='cursor-pointer hover:text-gray-400'>Projects</a>
                <a href='#Testimonials' className='cursor-pointer hover:text-gray-400'>Testimonials</a>
         </ul>
